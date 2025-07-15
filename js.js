b1c905338096c81620eb461b34cc93a969544031
Dev5304 <devyadav5304@gmail.com>
2025-07-15 09:43:16 +0530

    Deploy: initial commit

---
+++ js.js
@@ -0,0 +1,150 @@
+$('.first-cur').owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: false,
+  nav: false,
+  autoplay: true,
+  responsive:     {
+      0: {
+          items: 1
+      },
+      360: {
+        items: 2,
+    },
+      390: {
+        items: 2,
+    },
+      480: {
+        items: 3,
+    },
+      576: {
+          items: 4,
+      },
+      768: {
+          items: 5,
+      },
+      992: {
+          items: 5,
+      },
+  },
+
+})
+
+$('.second-cur').owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: false,
+  nav: false,
+  autoplay: true,
+  responsive:     {
+      0: {
+          items: 1
+      },
+      576: {
+          items: 1,
+      },
+      768: {
+          items: 2,
+      },
+      992: {
+          items: 2,
+      },
+  },
+
+})
+
+
+  $('.third-cur').owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: true,
+  nav: false,
+  autoplay: true,
+  responsive:     {
+      0: {
+          items: 1
+      },
+      576: {
+          items: 1,
+      },
+      768: {
+          items: 2,
+      },
+      992: {
+          items: 3,
+      },
+  },
+
+})
+
+
+
+$('.fouth-cur').owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: false,
+  nav: false,
+  autoplay: true,
+  responsive:     {
+      0: {
+          items: 1
+      },
+      576: {
+          items: 1,
+      },
+      768: {
+          items: 2,
+      },
+      992: {
+          items: 3,
+      },
+  },
+
+})
+
+
+$('.fifth-cur').owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: false,
+  nav: false,
+  autoplay: true,
+  responsive:     {
+      0: {
+          items: 1
+      },
+      576: {
+          items: 1,
+      },
+      768: {
+          items: 2,
+      },
+      992: {
+          items: 3,
+      },
+  },
+
+})
+
+$('.sixth-cur').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    nav: false,
+    autoplay: true,
+    responsive:     {
+        0: {
+            items: 1
+        },
+        576: {
+            items: 1,
+        },
+        768: {
+            items: 1,
+        },
+        992: {
+            items: 1,
+        },
+    },
+  
+  })
